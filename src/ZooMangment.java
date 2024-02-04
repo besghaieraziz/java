785497de5b7812b40fff7b8c17bd7052f3cffe7c
besghaieraziz <aziz.besghaier@esprit.tn>
2024-02-04 23:24:46 +0100

    second commit

--- src/ZooMangment.java
+++ src/ZooMangment.java
@@ -18,4 +18,41 @@ class ZooMangment {
 
     }
 
+}class Animal {
+    String family;
+    String name;
+    int age;
+    boolean isMammal;
+
+    public Animal(String family, String name, int age, boolean isMammal) {
+        this.family = family;
+        this.name = name;
+        this.age = age;
+        this.isMammal = isMammal;
+    }
 }
+
+class Zoo {
+    Animal[] animals;
+    String name;
+    String city;
+    int nbrCages;
+
+    public Zoo(String name, String city, int nbrCages) {
+        this.name = name;
+        this.city = city;
+        this.nbrCages = nbrCages;
+        this.animals = new Animal[25];
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Animal lion = new Animal("border", "lion", 5, true);
+        Zoo myZoo = new Zoo("zzh", "VilleZ", 10);
+
+        myZoo.animals[0] = lion;
+    }
+}
+
